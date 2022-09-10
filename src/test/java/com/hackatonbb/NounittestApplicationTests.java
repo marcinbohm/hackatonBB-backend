1d7f8e90de974cf5746e8b6a9a415e83a49073b5
marcin.bohm <marcin.bohm@cloudimed.pl>
2022-09-10 19:24:39 +0200

    Initial commit

---
+++ src/test/java/com/hackatonbb/NounittestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackatonbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NounittestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
